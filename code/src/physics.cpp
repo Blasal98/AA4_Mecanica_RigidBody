6e59aa0bd30c411fa179a15fcfb6c2c828ab8825
Victor Blasal <victor.blas@enti.cat>
2020-05-01 19:29:31 +0200

    es pinta el cub

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,5 +1,6 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <glm\gtc\matrix_transform.hpp>
 
 //Exemple
 extern void Exemple_GUI();
@@ -8,13 +9,39 @@ extern void Exemple_PhysicsUpdate(float dt);
 extern void Exemple_PhysicsCleanup();
 
 bool show_test_window = false;
+
+
+extern bool renderCube;
+namespace Cube {
+
+	extern void setupCube();
+	extern void cleanupCube();
+	extern void updateCube(const glm::mat4& transform);
+}
+
+
+void MyPhysicsInit() {
+	renderCube = true;
+	Cube::setupCube();
+}
+void MyPhysicsUpdate(float dt) {
+	if (renderCube) {
+		Cube::updateCube(glm::mat4(1.f));
+	}
+}
+void MyPhysicsCleanup() {
+	Cube::cleanupCube();
+}
+
+
+
+
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
-
 	{	
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
-		Exemple_GUI();
+		//Exemple_GUI();
 	}
 	
 	ImGui::End();
@@ -22,12 +49,15 @@ void GUI() {
 
 void PhysicsInit() {
 	//Exemple_PhysicsInit();
+	MyPhysicsInit();
 }
 
 void PhysicsUpdate(float dt) {
 	//Exemple_PhysicsUpdate(dt);
+	MyPhysicsUpdate(dt);
 }
 
 void PhysicsCleanup() {
 	//Exemple_PhysicsCleanup();
+	MyPhysicsCleanup();
 }

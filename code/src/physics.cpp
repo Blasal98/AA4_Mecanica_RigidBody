554c7878947fd36eb977bc8d55a098f542c7e8e2
Victor Blasal <victor.blas@enti.cat>
2020-05-02 10:17:13 +0200

    shecpoin

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -2,6 +2,7 @@
 #include <imgui\imgui_impl_sdl_gl3.h>
 #include <glm\gtc\matrix_transform.hpp>
 #include <time.h>
+#include <iostream>
 
 //Exemple
 extern void Exemple_GUI();
@@ -23,6 +24,16 @@ namespace Cube {
 	glm::vec3 scale = glm::vec3(1,1,1);
 	int mass = 1;
 	glm::vec3 gravity = glm::vec3(0,-9.81f,0) * (float)mass;
+	glm::mat3 inertia = glm::mat3(1/12,0,0,  0,1/12,0,  0,0,1/12);
+
+	glm::mat3 setupRotationMatrix(glm::vec3 rotationAngles) {
+		
+		glm::mat3 rotationX = glm::mat3(1,0,0,  0,glm::cos(rotationAngles.x),-glm::sin(rotationAngles.x),  0, glm::sin(rotationAngles.x), glm::cos(rotationAngles.x));
+		glm::mat3 rotationY = glm::mat3(glm::cos(rotationAngles.y),0, glm::sin(rotationAngles.y),  0,1,0,  -glm::sin(rotationAngles.y),0, glm::cos(rotationAngles.y));
+		glm::mat3 rotationZ = glm::mat3(glm::cos(rotationAngles.z), -glm::sin(rotationAngles.z),0,  glm::sin(rotationAngles.z), glm::cos(rotationAngles.z),0,  0,0,1);
+
+		return rotationX * rotationY * rotationZ;
+	}
 
 	struct CubeStruct {
 		glm::vec3 position;
@@ -33,15 +44,24 @@ namespace Cube {
 		glm::vec3 angularMomentum;
 		glm::vec3 torque;
 
+		glm::vec3 angularVelocity;
+
+		glm::mat3 inertiaMatrix;
+		glm::mat3 rotationMatrix;
+
 		void cubeReset() {
 			
 			position = glm::vec3(rand() % 7 - 3, rand() % 7 + 2, rand() % 7 - 3);
-			rotation = glm::vec3(glm::radians((float)(rand() % 360)), glm::radians((float)(rand() % 360)), glm::radians((float)(rand() % 360)));
+			//rotation = glm::vec3(glm::radians((float)(rand() % 360)), glm::radians((float)(rand() % 360)), glm::radians((float)(rand() % 360)));
+			rotation = glm::vec3(glm::radians(45.f),0,0);
 
 			velocity = glm::vec3(0, 0, 0);
 			linearMomentum = glm::vec3(0, 0, 0);
 			angularMomentum = glm::vec3(0, 0, 0);
 			torque = glm::vec3(0, 0, 0);
+
+			rotationMatrix = setupRotationMatrix(rotation);
+			inertiaMatrix = rotationMatrix * inertia * glm::transpose(rotationMatrix);
 		}
 
 		CubeStruct() {
@@ -81,6 +101,9 @@ void MyPhysicsUpdate(float dt) {
 		ourCube->velocity = ourCube->linearMomentum / (float)Cube::mass;
 		ourCube->position += dt * ourCube->velocity;
 
+		ourCube->rotationMatrix = Cube::setupRotationMatrix(ourCube->rotation);
+		ourCube->inertiaMatrix = ourCube->rotationMatrix * Cube::inertia * glm::transpose(ourCube->rotationMatrix);
+
 		glm::mat4 translation = glm::translate(glm::mat4(), ourCube->position);
 		glm::mat4 rotation = glm::mat4();
 		rotation = glm::rotate(rotation, ourCube->rotation.x, glm::vec3(1, 0, 0));
@@ -88,12 +111,12 @@ void MyPhysicsUpdate(float dt) {
 		rotation = glm::rotate(rotation, ourCube->rotation.z, glm::vec3(0, 0, 1));
 
 		Cube::updateCube(translation * rotation);
+		
 	}
 }
 void MyPhysicsCleanup() {
 	Cube::cleanupCube();
 }
-
 
 
 
@@ -122,3 +145,4 @@ void PhysicsCleanup() {
 	//Exemple_PhysicsCleanup();
 	MyPhysicsCleanup();
 }
+
